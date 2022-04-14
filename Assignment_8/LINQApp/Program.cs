91e3450c49c02c276927b562abe631980b14dcb7
Brandon Antillon <bantillon@pm.me>
2022-04-13 23:58:45 -0700

    ass 8 added 2.1 and 2.2

--- Assignment_8/LINQApp/Program.cs
+++ Assignment_8/LINQApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Xml.Serialization;
 
 namespace LINQApp
@@ -11,14 +12,13 @@ namespace LINQApp
     {
         static void Main(string[] args)
         {
-            ////////////////////
-            /// Question 2.1 ///
-            ////////////////////
-            
-            using (var textReader = new System.IO.StreamReader("App_Data/Courses.csv"))
+            using (var textReader = new StreamReader("App_Data/Courses.csv"))
             {
                 // set delimiter
                 char _Delimiter = ',';
+
+                // define empty list of course objects
+                List<Course> courseList = new List<Course>();
 
                 // start at the first line of the text file
                 string line = textReader.ReadLine();
@@ -30,29 +30,57 @@ namespace LINQApp
                     line = textReader.ReadLine(); skipCount++;
                 }
 
+                // while loop that iterates through all items of the courses
                 while (line != null)
                 {
                     // split columns by delimiter
                     String[] columns = line.Split(_Delimiter);
 
-                    // create course object with proper data
-                    Course course = new Course
-                    {
-                        CourseId = columns[3],
-                        Title = columns[2],
-                        CourseCode = columns[1],
-                        Location = columns[8],
-                        Instructor = columns[4]
-                    };
-
-                    XmlSerializer x = new XmlSerializer(course.GetType());
-
-                    x.Serialize(Console.Out, course);
-                    Console.WriteLine();
+                    // add course object to course list
+                    courseList.Add(
+                        new Course
+                        {
+                            CourseId = columns[3],
+                            Title = columns[2],
+                            CourseCode = columns[1],
+                            Location = columns[8],
+                            Instructor = columns[4]
+                        }
+                    );
 
                     line = textReader.ReadLine();
                 }
 
+
+
+                ////////////////////
+                /// Question 2.1 ///
+                ////////////////////
+                /// Creates in memory xml with course objects
+                
+                XmlRootAttribute rootNode = new XmlRootAttribute("Courses");
+                XmlSerializer courseSerializer = new XmlSerializer(courseList.GetType(), rootNode);
+
+                Console.WriteLine("In-memory array created with XML of course objects. \n");
+
+                ////////////////////
+                /// Question 2.2 ///
+                ////////////////////
+                // create xml file, add serialized XML content, and close it.
+
+                FileStream coursesFile = File.Create("App_Data/Courses.xml");
+                courseSerializer.Serialize(coursesFile, courseList);
+                coursesFile.Close();
+
+                Console.WriteLine(
+                    "Courses.xml written to {0} in current project directory. \n",
+                    Directory.GetCurrentDirectory() + "\\App_Data\\Courses.xml"
+                );
+
+
+
+
+                Console.WriteLine("Press ENTER to end program...");
                 Console.ReadLine();
             }
         }
