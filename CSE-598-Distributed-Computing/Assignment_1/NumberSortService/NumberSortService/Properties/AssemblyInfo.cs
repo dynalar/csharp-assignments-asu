5a2be45f8295e9eec1a55dffdab994a2d89e9524
Brandon <bantillon@pm.me>
2023-05-21 21:02:09 -0700

    reorganized project

--- CSE-598-Distributed-Computing/Assignment_1/NumberSortService/NumberSortService/Properties/AssemblyInfo.cs
+++ CSE-598-Distributed-Computing/Assignment_1/NumberSortService/NumberSortService/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TemperatureService")]
+[assembly: AssemblyTitle("NumberSortService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TemperatureService")]
+[assembly: AssemblyProduct("NumberSortService")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b14cf84d-8894-4673-9450-20b319efe00d")]
+[assembly: Guid("fa0b0ee1-e309-4c07-b1c6-d17f880a6386")]
 
 // Version information for an assembly consists of the following four values:
 //
