137cf4a599ea4429d0b8d3164dd7be2ad6a8f342
Brandon <bantillon@pm.me>
2023-05-28 10:46:25 -0700

    Added start of P2

---
+++ CSE-598-Distributed-Computing/Project_2/Assignment_3/ConsoleApp1/ComputerMaker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationScenario
+{
+    // create delegate for notify
+    public delegate void Notify();
+    internal class ComputerMaker
+    {
+        // constructor
+        public ComputerMaker() { }
+
+
+        // set limit of price cuts allowed
+        private const int TotalPriceCutsAllowed = 10;
+
+        // define a price cut event
+        public event Notify PriceChangeEvent;
+
+        // receive strings from multi cell buffer and call decoder to convert to order object
+
+        // call handlers in the stores if there is a price cut event
+
+        // call the pricing model to check the price
+
+        // every order received starts a new thread with OrderProcessing class
+
+        //
+
+        // receives an order and if it's valid, spins off a new thread to OrderProcessing class
+        public void sendOrder(string order)
+        {
+            // call decoder
+            Decoder decoder = new Decoder(order);
+        }
+    }
+}
---
+++ CSE-598-Distributed-Computing/Project_2/Assignment_3/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationScenario
+{
+    internal class Program
+    {
+        // main class that runs all of our operations
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
