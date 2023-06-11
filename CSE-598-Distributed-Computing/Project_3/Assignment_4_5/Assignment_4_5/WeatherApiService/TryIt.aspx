5764362648674824bdca257ad6f2a1c19147a551
Brandon <bantillon@pm.me>
2023-06-11 12:13:21 -0700

    stupid fucking forms

---
+++ CSE-598-Distributed-Computing/Project_3/Assignment_4_5/Assignment_4_5/WeatherApiService/TryIt.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="TryIt.aspx.cs" Inherits="Assignment_4_5.WeatherApiService.TryIt" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Enter Latitude and Longitude</h2>
+    <div>
+        <asp:TextBox ID="LatitudeTextBox" runat="server" placeholder="Latitude"></asp:TextBox>
+    </div>
+    <div>
+        <asp:TextBox ID="LongitudeTextBox" runat="server" placeholder="Longitude"></asp:TextBox>
+    </div>
+    <div>
+        <asp:Button ID="SubmitButton" runat="server" Text="Submit" OnClick="SubmitButton_Click" />
+    </div>
+    <div>
+        <asp:Literal ID="ResultLiteral" runat="server"></asp:Literal>
+    </div>
+</asp:Content>
---
+++ CSE-598-Distributed-Computing/Project_3/Assignment_4_5/Assignment_4_5/WeatherApiService/TryIt.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_4_5.WeatherApiService
+{
+    public partial class TryIt : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            // Get the values from the text boxes
+            string latitude = LatitudeTextBox.Text;
+            string longitude = LongitudeTextBox.Text;
+
+            // call the service reference to get our weather
+
+
+            // Display the result
+            ResultLiteral.Text = $"Latitude: {latitude}, Longitude: {longitude}";
+        }
+    }
+}
