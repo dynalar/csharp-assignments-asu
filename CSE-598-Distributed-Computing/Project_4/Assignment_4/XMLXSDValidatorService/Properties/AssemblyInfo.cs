a0e112b2069220cafabe9e4bb7609cfa525a88f7
Brandon <bantillon@pm.me>
2023-06-23 17:09:56 -0700

    added xml validato service

---
+++ CSE-598-Distributed-Computing/Project_4/Assignment_4/XMLXSDValidatorService/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XMLXSDValidatorService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XMLXSDValidatorService")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c3e509c2-df5f-42c9-a069-4972dceaf803")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSE-598-Distributed-Computing/Project_4/Assignment_4/XMLXSDValidatorService/XMLXSDValidatorService.svc.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace XMLXSDValidatorService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class XMLXSDValidatorService : IXMLXSDValidatorService
+    {
+        /// <summary>
+        /// Endpoint that validates XML to an XSD file.
+        /// </summary>
+        /// <param name="xmlUrl"></param>
+        /// <param name="xsdUrl"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public string ValidateXMLToXSD(string xmlUrl, string xsdUrl)
+        {
+            // get validation status, as a bool
+            try
+            {
+                bool validationStatus = ValidateXmlToXsd(xmlUrl, xsdUrl);
+                if (validationStatus)
+                {
+                    return "Success!";
+                }
+                else
+                {
+                    return "False";
+                }
+            } catch(Exception e)
+            {
+                return "Failed to validate XML against XSD. Reason: " + e.Message;
+            }
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public bool ValidateXmlToXsd(string xmlUrl, string xsdUrl)
+        {
+            try
+            {
+                // pull both of our files 
+                string xmlContent;
+                using (WebClient xmlClient = new WebClient())
+                {
+                    xmlContent = xmlClient.DownloadString(xmlUrl);
+                }
+
+                // Download the XSD schema file
+                string xsdContent;
+                using (WebClient xsdClient = new WebClient())
+                {
+                    xsdContent = xsdClient.DownloadString(xsdUrl);
+                }
+
+                // Create an XmlSchemaSet and add the XSD schema to it
+                XmlSchemaSet schemaSet = new XmlSchemaSet();
+                schemaSet.Add("", XmlReader.Create(new StringReader(xsdContent)));
+
+                // Create XmlReaderSettings and specify the schema set for validation
+                XmlReaderSettings readerSettings = new XmlReaderSettings();
+                readerSettings.ValidationType = ValidationType.Schema;
+                readerSettings.Schemas = schemaSet;
+
+                // Create an XmlReader for the XML content
+                using (XmlReader reader = XmlReader.Create(new StringReader(xmlContent), readerSettings))
+                {
+                    // Read the XML content, performing the validation
+                    while (reader.Read())
+                    {
+                        // Perform validation here (if required)
+                    }
+                }
+
+                // If no exception occurred during validation, the XML is valid
+                return true;
+            } catch(Exception e)
+            {
+                // Handle any exceptions that occurred during validation
+                Console.WriteLine("XML validation error: " + e.Message);
+                return false;
+            }
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
